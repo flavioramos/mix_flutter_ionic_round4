a521732b8afee68c73e0f8c5efd521a0f8005c8f
Flavio Augusto Silva Ramos <flavio.augusto.ramos@accenture.com>
2021-09-22 17:41:09 -0300

    added home activity to ionic_project1
    renamed app to appIonicProject1
    
    added home into new app module
    
    organized manifest file and gradle

---
+++ ionic_project1/platforms/android/appIonicCaller/src/main/java/com/example/appioniccaller/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.example.appioniccaller;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import io.ionic.starter.MainActivity;
+
+public class HomeActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_home);
+  }
+
+  public void openProject1(View view) {
+    startActivity(new Intent(this, MainActivity.class));
+  }
+
+  public void openProject2(View view) {
+
+  }
+}
